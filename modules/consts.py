07951e8e301b572aa945cc30510ccdebe8c332b9
Isla Walker <github@islawalker.uk>
2025-06-15 16:59:58 +0100

    fix: get CWD properly i am stupid stupid stupid (stupid x3)

--- modules/consts.py
+++ modules/consts.py
@@ -1,5 +1,6 @@
 import pathlib
 import tempfile
+import os
 
 patcher_url = "https://patcher.wiilink24.com"
 patcher_version = "1.1.2 Nightly"
@@ -7,10 +8,7 @@ patcher_version = "1.1.2 Nightly"
 temp_dir = pathlib.Path().joinpath(tempfile.gettempdir(), "WiiLinkPatcher")
 file_path = pathlib.Path(__file__).parents[1]
 
-if file_path.resolve().as_posix().endswith(".app/Contents/MacOS"):
-    output_path = file_path.parents[2]
-else:
-    output_path = pathlib.Path()
+output_path = pathlib.Path(os.getcwd())
 
 wiilink_dir = output_path.joinpath("WiiLink")
 
