44d0ed51f2ed27d8159120cdf0ec50df05894fd4
Harry Walker <contact@harrywalker.uk>
2026-03-01 11:12:48 +0000

    chore: check nuitka build

--- src/WiiLinkPatcherGUI/modules/consts.py
+++ src/WiiLinkPatcherGUI/modules/consts.py
@@ -10,5 +10,7 @@ patcher_version = "1.3.2 Nightly"
 
 temp_dir = pathlib.Path().joinpath(tempfile.gettempdir(), "WiiLinkPatcher")
 file_path = pathlib.Path(__file__).parents[1]
-if platform.system() == "Darwin":
+
+# File location is different in macOS Nuitka builds
+if platform.system() == "Darwin" and "__compiled__" in globals():
     file_path = file_path.parent
