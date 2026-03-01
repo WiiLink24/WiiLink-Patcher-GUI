e374aec45eeafc46a0956105a28775495a11c4d2
Harry Walker <contact@harrywalker.uk>
2026-03-01 11:01:09 +0000

    fix(macos): set file path properly

--- src/WiiLinkPatcherGUI/modules/consts.py
+++ src/WiiLinkPatcherGUI/modules/consts.py
@@ -3,10 +3,12 @@
 
 import pathlib
 import tempfile
+import platform
 
 patcher_url = "https://patcher.wiilink24.com"
 patcher_version = "1.3.2 Nightly"
 
 temp_dir = pathlib.Path().joinpath(tempfile.gettempdir(), "WiiLinkPatcher")
 file_path = pathlib.Path(__file__).parents[1]
-print(file_path)
+if platform.system() == "Darwin":
+    file_path = file_path.parent
